28cdef75d08719d329717847e523a6154ec4e688
Sofia Nguy <sofianguy@gmail.com>
2016-05-15 14:04:24 -0700

    create class with HashMap

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,23 @@
+import java.util.HashMap;
+
 /**
  * Created by sofia on 5/14/16.
  */
+
 public class HelloWorld {
     public static void main(String[] arg) {
-        System.out.println("hello world");
+        RestaurantMenu menu1 = new RestaurantMenu();
+        RestaurantMenu menu2 = new RestaurantMenu();
+        menu1.restaurantTitle = "Restaurant ABC";
+        menu1.menuItems.put("sandwich", 10);
+        menu1.menuItems.put("salad", 8);
+        menu2.restaurantTitle = "Restaurant XYZ";
+        menu2.menuItems.put("sushi", 15);
+        menu2.menuItems.put("teriyaki chicken", 12);
+
+        for (String item : menu1.menuItems.keySet()) {
+            System.out.println(item);
+        }
     }
+
 }
---
+++ src/RestaurantMenu.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by sofia on 5/15/16.
+ */
+public class RestaurantMenu {
+
+    HashMap<String, Integer> menuItems;
+    String restaurantTitle;
+
+    public RestaurantMenu() {
+        menuItems = new HashMap<String, Integer>();
+    }
+}
