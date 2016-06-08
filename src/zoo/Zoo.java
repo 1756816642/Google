73d214d5ea24247837f530f4a69b5f0f4ac03588
Sofia Nguy <sofianguy@gmail.com>
2016-06-07 17:17:05 -0700

    add AquaticAnimals array list to zoo class

--- src/zoo/Zoo.java
+++ src/zoo/Zoo.java
@@ -9,10 +9,12 @@ public class Zoo {
     public ArrayList<Mammal> mammals;
     public ArrayList<Reptile> reptiles;
 //    public ArrayList<Reptile> reptiles = new ArrayList<Reptile>();
+    public ArrayList<AquaticAnimal> aquaticAnimals;
 
     public Zoo() {
         mammals = new ArrayList<Mammal>();
         reptiles = new ArrayList<Reptile>();
+        aquaticAnimals = new ArrayList<AquaticAnimal>();
     }
 
     public void addAnimals(Mammal mammal) {
@@ -21,6 +23,10 @@ public class Zoo {
 
     public void addAnimals(Reptile reptile) {
         reptiles.add(reptile);
+    }
+
+    public void addAnimals(AquaticAnimal aquaticAnimal) {
+        aquaticAnimals.add(aquaticAnimal);
     }
 
 }
